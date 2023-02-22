a527dfee7a98f6fe18fa551ff855eee8e5a67df6
Alexis Guzman <codingcodax@gmail.com>
2023-02-22 13:34:42 -0600

    feat(helpers): export create parent dir if necessary

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,2 +1,3 @@
+export { default as createParentDirIfNecessary } from './create-parent-dir-if-necessary.helper';
 export { default as getConfig } from './get-config.helper';
 export { default as logIntro } from './log-intro.helper';
