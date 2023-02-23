d1002b64a8395223519bd0426a62caf88314b578
Alexis Guzman <codingcodax@gmail.com>
2023-02-22 20:54:48 -0600

    feat(helpers): create log error

---
+++ src/helpers/log-error.helper.ts
@@ -0,0 +1,10 @@
+import chalk from 'chalk';
+
+const logError = (error: string) => {
+  console.info('\n');
+  console.info(chalk.bold.hex('#e5484d')('Error creating component.'));
+  console.info(chalk.hex('#e5484d')(error));
+  console.info('\n');
+};
+
+export default logError;
