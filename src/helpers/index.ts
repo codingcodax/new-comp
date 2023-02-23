c184674edbda68eabad3494dd8e620e13d831fd9
Alexis Guzman <codingcodax@gmail.com>
2023-02-22 21:28:12 -0600

    feat(helpers): export log conclusion

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,4 +2,5 @@ export { default as buildPrettifier } from './build-prettifier.helper';
 export { default as checkIfComponentExists } from './check-if-component-exists.helper';
 export { default as createParentDirIfNecessary } from './create-parent-dir-if-necessary.helper';
 export { default as getConfig } from './get-config.helper';
+export { default as logConclusion } from './log-conclusion.helper';
 export { default as logIntro } from './log-intro.helper';
