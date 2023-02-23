77e848df2dcf31b5cf14ea81a0b962ac61b892bb
Alexis Guzman <codingcodax@gmail.com>
2023-02-22 20:55:46 -0600

    feat(helpers): export check dir if exists

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,4 @@
+export { default as checkIfComponentExists } from './check-if-component-exists.helper';
 export { default as createParentDirIfNecessary } from './create-parent-dir-if-necessary.helper';
 export { default as getConfig } from './get-config.helper';
 export { default as logIntro } from './log-intro.helper';
