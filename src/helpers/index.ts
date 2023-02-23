cbdf15390bd2f7440592559e0e80abecddf24502
Alexis Guzman <codingcodax@gmail.com>
2023-02-22 21:12:09 -0600

    feat(helpers): export build prettifier

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,4 @@
+export { default as buildPrettifier } from './build-prettifier.helper';
 export { default as checkIfComponentExists } from './check-if-component-exists.helper';
 export { default as createParentDirIfNecessary } from './create-parent-dir-if-necessary.helper';
 export { default as getConfig } from './get-config.helper';
