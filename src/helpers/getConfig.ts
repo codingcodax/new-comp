db6940366f5f0fa6be063b118413d1da034c79f1
Alexis Guzman <codingcodax@gmail.com>
2024-04-13 12:08:02 -0600

    feat(helpers): create get config function

---
+++ src/helpers/getConfig.ts
@@ -0,0 +1,26 @@
+import { homedir } from 'os';
+import { requireOptional } from '~/utils/requireOptional';
+
+type Config = {
+  lang: string;
+  type: string;
+  ui: string;
+  dir: string;
+};
+
+export const getConfig = () => {
+  const home = homedir();
+  const currentPath = process.cwd();
+
+  const defaults: Config = {
+    lang: 'ts',
+    type: 'function',
+    ui: 'vanilla',
+    dir: 'src/components',
+  };
+
+  const globalOverrides: Config = requireOptional(`/${home}/nw.config.json`);
+  const localOverrides: Config = requireOptional(`/${currentPath}/nw.config.json`);
+
+  return Object.assign({}, defaults, globalOverrides, localOverrides);
+};
