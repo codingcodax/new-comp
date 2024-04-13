509832290d030f57258031d7f24ee6f37600bce5
Alexis Guzman <codingcodax@gmail.com>
2024-04-13 05:59:51 -0600

    chore: reset

--- src/helpers/log-conclusion.helper.ts
+++
@@ -1,8 +0,0 @@
-import chalk from 'chalk';
-
-const logConclusion = () => {
-  console.info('\n');
-  console.info(chalk.bold.hex('#30a46c')('Component successfully created!'));
-};
-
-export default logConclusion;
