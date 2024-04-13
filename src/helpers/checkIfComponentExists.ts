6239d65cb9f547042b3d9edeac4c9819280cc081
Alexis Guzman <codingcodax@gmail.com>
2024-04-13 16:18:38 -0600

    feat(helpers): create check if component exists function

---
+++ src/helpers/checkIfComponentExists.ts
@@ -0,0 +1,15 @@
+import { existsSync } from 'fs';
+import path from 'path';
+
+import { logError } from './logError';
+
+export const checkIfComponentExists = (dir: string) => {
+  const fullPathToComponentDir = path.resolve(dir);
+
+  if (existsSync(fullPathToComponentDir)) {
+    logError(
+      `Looks like this component already exists! There's already a component at ${dir}. \nPlease delete this directory and try again.`
+    );
+    process.exit(0);
+  }
+};
