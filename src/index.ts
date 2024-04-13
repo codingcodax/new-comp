ba7a9692e6271bff7dd758d78c0f77a53d95662e
Alexis Guzman <codingcodax@gmail.com>
2024-04-13 16:19:49 -0600

    feat: add helper functions

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 #!/usr/bin/env node
-import { readFileSync } from 'fs';
+import { mkdirSync, readFileSync, writeFileSync } from 'fs';
 import path from 'path';
 import { Command, Option } from 'commander';
 
 import { getConfig } from '~/helpers/getConfig';
 
 import packageJson from '../package.json';
+import { buildPrettifier } from './helpers/buildPrettifier';
+import { checkIfComponentExists } from './helpers/checkIfComponentExists';
+import { logConclusion } from './helpers/logConclusion';
+import { logIntro } from './helpers/logIntro';
 
 const config = getConfig();
 const program = new Command();
+const prettify = buildPrettifier();
 
 const initCommand = (): void => {
   program
@@ -51,26 +56,49 @@ const initCommand = (): void => {
   const fileExtension = options.lang === 'js' ? 'js' : 'tsx';
   const indexExtension = options.lang === 'js' ? 'js' : 'ts';
   const fileTemplatePath = `${options.type}/${options.ui}.tsx`;
-  const indexTemplatePath = 'index.tsx';
+  const indexTemplatePath = 'index.txt';
 
   const fileTemplateFinalPath = path.join(
     __dirname.replace('lib', 'templates'),
     fileTemplatePath
   );
+  const indexTemplateFinalPath = path.join(
+    __dirname.replace('lib', 'templates'),
+    indexTemplatePath
+  );
   const fileTemplateContent = readFileSync(
     fileTemplateFinalPath,
     'utf-8'
   ).replaceAll('COMPONENT_NAME', componentName);
+  const indexTemplateContent = readFileSync(
+    indexTemplateFinalPath,
+    'utf-8'
+  ).replace('COMPONENT_NAME', componentName);
 
-  console.log({
-    componentName,
-    options,
-    fileExtension,
-    indexExtension,
-    fileTemplatePath,
-    indexTemplatePath,
-    fileTemplateContent,
+  // Get all of our file paths worked out, for the user's project.
+  const componentDir = `${options.dir}/${componentName}`;
+  const filePath = `${componentDir}/${componentName}.${fileExtension}`;
+  const indexPath = `${componentDir}/index.${indexExtension}`;
+
+  logIntro({
+    name: componentName,
+    lang: options.lang,
+    type: options.type,
+    ui: options.ui,
+    dir: componentDir,
   });
+
+  // Check if the component has already been created.
+  checkIfComponentExists(componentDir);
+
+  // Start by creating the directory that our component lives in.
+  mkdirSync(componentDir, { recursive: true });
+
+  // Create component and index files coping content from templates.
+  writeFileSync(filePath, prettify(fileTemplateContent), 'utf-8');
+  writeFileSync(indexPath, prettify(indexTemplateContent), 'utf-8');
+
+  logConclusion();
 };
 
 initCommand();
