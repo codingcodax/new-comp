e990f00a1f2351865db3ece8cde3a1d170ff9807
Alexis Guzman <codingcodax@gmail.com>
2024-04-13 07:51:23 -0600

    feat(prettier): add config for plugin

--- prettier.config.js
+++ prettier.config.js
@@ -3,6 +3,19 @@ const config = {
   singleQuote: true,
   tabWidth: 2,
   trailingComma: 'es5',
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports'
+  ],
+  importOrder: [
+    '<TYPES>',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '<TYPES>^[.|..|~]',
+    '^~/',
+    '',
+    '^[../]',
+    '^[./]',
+  ]
 }
 
 export default config;
