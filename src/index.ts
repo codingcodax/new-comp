a7e35b010ffe8e91964f1c076d741062010f072d
Alexis Guzman <codingcodax@gmail.com>
2024-04-14 09:23:16 -0600

    feat: add clack for prompts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { mkdirSync, readFileSync, writeFileSync } from 'fs';
 import path from 'path';
+import * as p from '@clack/prompts';
 import { Command, Option } from 'commander';
 
 import { buildPrettifier } from '~/helpers/buildPrettifier.ts';
@@ -15,14 +16,14 @@ const config = getConfig();
 const program = new Command();
 const prettify = buildPrettifier();
 
-const initCommand = (): void => {
+const initCommand = async () => {
   program
     .name(packageJson.name)
     .summary('Create react components')
     .description(packageJson.description)
     .version(packageJson.version, '-v, --version', 'Display version number')
     .helpOption('-h, --help', 'Display help for command')
-    .argument('<componentName>', 'Name of the component you want to create')
+    .argument('[componentName]', 'Name of the component you want to create')
     .addOption(
       new Option('-l, --lang <language>', 'Which laguange to use')
         .default(config.lang)
@@ -53,9 +54,74 @@ const initCommand = (): void => {
   const [componentName] = program.args;
   const options = program.opts();
 
-  const fileExtension = options.lang === 'js' ? 'js' : 'tsx';
-  const indexExtension = options.lang === 'js' ? 'js' : 'ts';
-  const fileTemplatePath = `${options.type}/${options.ui}.tsx`;
+  const project = await p.group(
+    {
+      ...(!componentName && {
+        name: () =>
+          p.text({
+            message: 'What is the name of your component?',
+            placeholder: 'MyComponent',
+            validate: (value) =>
+              value.length > 0 ? '' : 'Please enter a name',
+          }),
+      }),
+      ...(!options.lang && {
+        lang: () =>
+          p.select({
+            message: 'Which language would you like to use?',
+            initialValue: 'ts',
+            options: [
+              { value: 'ts', label: 'TypeScript' },
+              { value: 'js', label: 'JavaScript' },
+            ],
+          }),
+      }),
+      ...(!options.type && {
+        type: () =>
+          p.select({
+            message: 'What type of component would you like to create?',
+            initialValue: 'function',
+            options: [
+              { value: 'function', label: 'Function' },
+              { value: 'arrow', label: 'Arrow Function' },
+              { value: 'class', label: 'Class Component' },
+            ],
+          }),
+      }),
+      ...(!options.ui && {
+        ui: () =>
+          p.select({
+            message: 'Choose your favorite component library',
+            initialValue: 'vanilla',
+            options: [
+              { value: 'vanilla', label: 'Vanilla JS' },
+              { value: 'chakra', label: 'Chakra UI' },
+              { value: 'mui', label: 'Material UI' },
+            ],
+          }),
+      }),
+      ...(!options.dir && {
+        dir: () =>
+          p.text({
+            message: 'Where would you like to create your component?',
+            placeholder: 'components',
+            defaultValue: 'src/components',
+            validate: (value) =>
+              value.length > 0 ? '' : 'Please enter a directory',
+          }),
+      }),
+    },
+    {
+      onCancel: () => {
+        p.cancel('Operation cancelled');
+        process.exit(0);
+      },
+    }
+  );
+
+  const fileExtension = (project ?? options).lang === 'js' ? 'js' : 'tsx';
+  const indexExtension = (project ?? options).lang === 'js' ? 'js' : 'ts';
+  const fileTemplatePath = `${(project ?? options).type}/${(project ?? options).ui}.tsx`;
   const indexTemplatePath = 'index.txt';
 
   const fileTemplateFinalPath = path.join(
@@ -69,22 +135,22 @@ const initCommand = (): void => {
   const fileTemplateContent = readFileSync(
     fileTemplateFinalPath,
     'utf-8'
-  ).replaceAll('COMPONENT_NAME', componentName);
+  ).replaceAll('COMPONENT_NAME', project.name ?? componentName);
   const indexTemplateContent = readFileSync(
     indexTemplateFinalPath,
     'utf-8'
-  ).replace('COMPONENT_NAME', componentName);
+  ).replace('COMPONENT_NAME', project.name ?? componentName);
 
   // Get all of our file paths worked out, for the user's project.
-  const componentDir = `${options.dir}/${componentName}`;
-  const filePath = `${componentDir}/${componentName}.${fileExtension}`;
+  const componentDir = `${(project ?? options).dir}/${project.name ?? componentName}`;
+  const filePath = `${componentDir}/${project.name ?? componentName}.${fileExtension}`;
   const indexPath = `${componentDir}/index.${indexExtension}`;
 
   logIntro({
-    name: componentName,
-    lang: options.lang,
-    type: options.type,
-    ui: options.ui,
+    name: project.name ?? componentName,
+    lang: (project ?? options).lang as string,
+    type: (project ?? options).type as string,
+    ui: (project ?? options).ui as string,
     dir: componentDir,
   });
 
